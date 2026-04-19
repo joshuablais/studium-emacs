d9f93907ac71f34cd48c13999a6263801817aae8
Joshua Blais <josh@joshblais.com>
2026-04-19 13:38:17 -0600

    browser in eww

--- lisp/browser.el
+++ lisp/browser.el
@@ -3,7 +3,19 @@
 ;; set specific browser to open links
 ;; set browser to firefox
 ;; (setq browse-url-browser-function 'browse-url-firefox)
-(setq browse-url-browser-function 'browse-url-generic)
-(setq browse-url-generic-program "chromium")  ; replace with actual executable name
+;; (setq browse-url-browser-function 'browse-url-generic)
+;; (setq browse-url-generic-program "chromium")
+
+(defun my-browse-url-mpv (url &rest _args)
+  "Open URL in mpv."
+  (start-process "mpv" nil "mpv" url))
+
+(setq browse-url-handlers
+      '(("\\(youtube\\.com\\|youtu\\.be\\)" . my-browse-url-mpv)
+        ("." . eww-browse-url)))
+
+;; Keep your fallback setting
+(setq browse-url-secondary-browser-function 'browse-url-generic
+      browse-url-generic-program "chromium")
 
 (provide 'browser)
