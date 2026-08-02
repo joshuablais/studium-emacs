fb974cbd406fc32e7703528bc6cc5e3b822d0fd6
Joshua Blais <josh@joshblais.com>
2026-08-02 13:04:58 -0600

    meow

--- lisp/meow-setup.el
+++ lisp/meow-setup.el
@@ -56,7 +56,7 @@
    ;; Miscellaneous
    '("!" . jb/run-command)
    '("o t" . jb/ghostel)
-   '("o T" . vterm)
+   '("o T" . ghostel)
    '("o C" . jb/checks)
    '("o D" . jb/download-media)
    '("s T" . powerthesaurus-lookup-synonyms-dwim)
@@ -232,6 +232,7 @@
           (elfeed-show-mode . motion)
           (erc-mode . insert)
           (vterm-mode . insert)
+          (ghostel-mode . insert)
           (pdf-view-mode . motion)
           (calibredb-search-mode . motion)
           (dirvish-mode . motion)
