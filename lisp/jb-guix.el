0339d74a69b1939124d9ed7301ce4de2a4a271c4
Joshua Blais <josh@joshblais.com>
2026-08-19 19:48:13 -0600

    guile setup

--- lisp/jb-guix.el
+++ lisp/jb-guix.el
@@ -1,13 +1,21 @@
 ;;; jb-guix.el -*- lexical-binding: t; -*-
 (use-package geiser
-  :ensure nil)
+  :ensure nil
+  :demand t)
+
 (use-package geiser-guile
   :ensure nil
+  :demand t
   :config
-  (setq geiser-guile-case-sensitive-p t))
+  (setq geiser-guile-binary (or (executable-find "guile")
+                                "/home/joshua/.guix-profile/bin/guile")
+        geiser-guile-case-sensitive-p t
+        geiser-active-implementations '(guile)))
+
 (use-package guix
   :ensure nil
   :commands (guix guix-installed-user-packages guix-packages-by-name)
   :init
   (require 'guix nil t))
+
 (provide 'jb-guix)
